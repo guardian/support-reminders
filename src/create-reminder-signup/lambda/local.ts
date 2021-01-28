116731b585df057880d3b33b5657fc41f018d4b7
Tom Forbes <tom.forbes@theguardian.com>
2021-01-28 11:12:37 +0000

    input

--- src/create-reminder-signup/lambda/local.ts
+++ src/create-reminder-signup/lambda/local.ts
@@ -11,7 +11,7 @@ function run() {
 
 	const event = {
 		body: JSON.stringify({
-			emal: 'test-reminders10@theguardian.com',
+			email: 'test-reminders10@theguardian.com',
 			reminderPeriod: '2021-01',
 			reminderPlatform: 'WEB',
 			reminderComponent: 'EPIC',
