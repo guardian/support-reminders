080d58c1bf3b7c4e49eb1e6cfb51b67aac464fde
Tom Pretty <tompretty2@gmail.com>
2021-02-04 14:25:54 +0000

    Export recurring reminder signups

--- src/signup-exports/lambda/lambda.ts
+++ src/signup-exports/lambda/lambda.ts
@@ -3,17 +3,31 @@ import * as SSM from 'aws-sdk/clients/ssm';
 import { createDatabaseConnectionPool } from '../../lib/db';
 import { getDatabaseParamsFromSSM } from '../../lib/ssm';
 import { uploadAsCsvToS3 } from '../../lib/upload';
-import { getCreatedOrCancelledSignupsFromYesterday } from '../lib/db';
+import {
+	getCreatedOrCancelledOneOffSignupsFromYesterday,
+	getCreatedOrCancelledRecurringSignupsFromYesterday,
+} from '../lib/db';
 import { getYesterday } from '../lib/utils';
 
 const ssm: SSM = new AWS.SSM({ region: 'eu-west-1' });
 
 const yesterday = getYesterday();
-const S3_KEY = `one-off-signups/date=${yesterday}/one-off-signups.csv`;
+const ONE_OFF_S3_KEY = `one-off-signups/date=${yesterday}/one-off-signups.csv`;
+const RECURRING_S3_KEY = `recurring-signups/date=${yesterday}/recurring-signups.csv`;
 const S3_BUCKET = process.env.Bucket ?? '';
 
-export const handler = async (): Promise<number> =>
-	getDatabaseParamsFromSSM(ssm)
-		.then(createDatabaseConnectionPool)
-		.then(getCreatedOrCancelledSignupsFromYesterday)
-		.then((result) => uploadAsCsvToS3(result, S3_BUCKET, S3_KEY));
+export const handler = async (): Promise<number> => {
+	const pool = await getDatabaseParamsFromSSM(ssm).then(
+		createDatabaseConnectionPool,
+	);
+
+	const numOneOffs = await getCreatedOrCancelledOneOffSignupsFromYesterday(
+		pool,
+	).then((result) => uploadAsCsvToS3(result, S3_BUCKET, ONE_OFF_S3_KEY));
+
+	const numRecurrings = await getCreatedOrCancelledRecurringSignupsFromYesterday(
+		pool,
+	).then((result) => uploadAsCsvToS3(result, S3_BUCKET, RECURRING_S3_KEY));
+
+	return numOneOffs + numRecurrings;
+};
--- src/signup-exports/lib/db.ts
+++ src/signup-exports/lib/db.ts
@@ -2,7 +2,7 @@ import { Pool, QueryConfig, QueryResult } from 'pg';
 import { runWithLogging } from '../../lib/db';
 import { getYesterday } from './utils';
 
-export function getCreatedOrCancelledSignupsFromYesterday(
+export function getCreatedOrCancelledOneOffSignupsFromYesterday(
 	pool: Pool,
 ): Promise<QueryResult> {
 	const yesterday = getYesterday();
@@ -20,6 +20,33 @@ export function getCreatedOrCancelledSignupsFromYesterday(
 				reminder_stage,
 				reminder_option
 			FROM one_off_reminder_signups
+			WHERE DATE(reminder_created_at) = DATE($1)
+				OR DATE(reminder_cancelled_at) = DATE($1)
+        `,
+		values: [yesterday],
+	};
+
+	return runWithLogging(query, pool);
+}
+
+export function getCreatedOrCancelledRecurringSignupsFromYesterday(
+	pool: Pool,
+): Promise<QueryResult> {
+	const yesterday = getYesterday();
+
+	const query: QueryConfig = {
+		text: `
+			SELECT
+				identity_id,
+				country,
+				reminder_frequency_months,
+				reminder_created_at::text,
+				reminder_cancelled_at::text,
+				reminder_platform,
+				reminder_component,
+				reminder_stage,
+				reminder_option
+			FROM recurring_reminder_signups
 			WHERE DATE(reminder_created_at) = DATE($1)
 				OR DATE(reminder_cancelled_at) = DATE($1)
         `,
