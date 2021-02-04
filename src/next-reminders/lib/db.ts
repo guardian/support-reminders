1dbedb20fad30354baf8cf4b83115bc603b2c690
Tom Pretty <tompretty2@gmail.com>
2021-02-04 11:34:34 +0000

    Add next-reminders lambda (#9)
    * Add next-reminders lambda

---
+++ src/next-reminders/lib/db.ts
@@ -0,0 +1,27 @@
+import { Pool, QueryConfig, QueryResult } from 'pg';
+import { runWithLogging } from '../../lib/db';
+import { getCurrentReminderPeriod } from './utils';
+
+export function getNextReminders(pool: Pool): Promise<QueryResult> {
+	const currentReminderPeriod = getCurrentReminderPeriod();
+
+	const query: QueryConfig = {
+		text: `
+			SELECT
+				identity_id,
+				reminder_period::text,
+				reminder_created_at::text,
+				reminder_platform,
+				reminder_component,
+				reminder_stage,
+				reminder_option,
+				'ONE_OFF' as reminder_type
+			FROM one_off_reminder_signups
+			WHERE reminder_period = $1
+				AND reminder_cancelled_at IS NULL
+        `,
+		values: [currentReminderPeriod],
+	};
+
+	return runWithLogging(query, pool);
+}
---
+++ src/next-reminders/lib/utils.ts
@@ -0,0 +1,7 @@
+export function getCurrentReminderPeriod(): string {
+	const now = new Date();
+	const year = now.getFullYear().toString();
+	const month = (now.getMonth() + 1).toString().padStart(2, '0');
+
+	return `${year}-${month}-01`;
+}
