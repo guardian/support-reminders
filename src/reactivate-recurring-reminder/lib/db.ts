ff99b749ebb6d01e35d61ede7cbfa5256e57fd5a
Tom Pretty <tompretty2@gmail.com>
2021-02-23 10:19:55 +0000

    Set reminder_created_at when reactivating

--- src/reactivate-recurring-reminder/lib/db.ts
+++ src/reactivate-recurring-reminder/lib/db.ts
@@ -6,17 +6,20 @@ export async function reactivateRecurringReminder(
 	reactivation: Reactivation,
 	pool: Pool,
 ): Promise<QueryResult> {
+	const now = new Date();
+
 	const query: QueryConfig = {
 		text: `
 			UPDATE
 				recurring_reminder_signups
 			SET
+				reminder_created_at = $2,
 				reminder_cancelled_at = NULL
 			WHERE
 				identity_id = $1
 				AND reminder_cancelled_at IS NOT NULL
         `,
-		values: [reactivation.identity_id],
+		values: [reactivation.identity_id, now.toISOString()],
 	};
 	return runWithLogging(query, pool);
 }
