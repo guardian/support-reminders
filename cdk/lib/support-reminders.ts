d6d725f9af02c7e07749483ab038551ffc65d2a8
Michael Jacobson <michael.jacobson@guardian.co.uk>
2022-12-07 15:12:11 +0000

    update riff-raff and begin wiring up ci

--- cdk/lib/support-reminders.ts
+++ cdk/lib/support-reminders.ts
@@ -7,7 +7,7 @@ import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
 export class SupportReminders extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
-    const yamlTemplateFilePath = join(__dirname, "../..", "cfn-processed.yaml");
+    const yamlTemplateFilePath = join(__dirname, "../..", "cfn.yaml");
     new CfnInclude(this, "YamlTemplate", {
       templateFile: yamlTemplateFilePath,
     });
