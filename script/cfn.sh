761dd51b4f5fdd86e33b5e03e7ab289697ad04c9
Michael Jacobson <michael.jacobson@guardian.co.uk>
2022-12-19 17:46:05 +0000

    update CI workflow

---
+++ script/cfn.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR="${DIR}/.."
+cd "$ROOT_DIR/cdk"
+
+yarn clean
+yarn install
+yarn tsc
+yarn lint
+yarn test
+yarn synth
