ee6889cdcdc5e6fc0272c081e93ef69ad891aee7
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-01-16 15:04:53 +0000

    committing work in progress to version control

--- cdk/lib/support-reminders.ts
+++ cdk/lib/support-reminders.ts
@@ -1,75 +1,37 @@
+import { join } from "path";
 import { GuApiGatewayWithLambdaByPath, GuScheduledLambda} from "@guardian/cdk";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import {GuStack, GuStringParameter, GuSubnetListParameter} from "@guardian/cdk/lib/constructs/core";
+import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import { GuLambdaFunction } from "@guardian/cdk/lib/constructs/lambda";
 import type { App } from "aws-cdk-lib";
+import { CfnBasePathMapping, CfnDomainName } from "aws-cdk-lib/aws-apigateway";
 import { Schedule } from "aws-cdk-lib/aws-events";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
-// import { GuCname } from "@guardian/cdk/lib/constructs/dns";
+import { CfnRecordSetGroup } from "aws-cdk-lib/aws-route53";
+import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
+
+export interface SupportRemindersProps extends GuStackProps {
+	certificateId: string;
+	domainName: string;
+	hostedZoneId: string;
+}
 
 export class SupportReminders extends GuStack {
-	constructor(scope: App, id: string, props: GuStackProps) {
+	constructor(scope: App, id: string, props: SupportRemindersProps) {
 		super(scope, id, props);
 
-		// ---- String Parameters ---- //
-		new GuStringParameter(
-			this,
-			"CertificateArn",
-			{
-				description:
-					"ARN of the certificate",
-			}
-		);
+		// ---- Existing CFN template ---- //
+		const yamlTemplateFilePath = join(__dirname, "../..", "cfn.yaml");
+		new CfnInclude(this, "YamlTemplate", {
+			templateFile: yamlTemplateFilePath,
+		});
 
-		new GuStringParameter(
-			this,
-			"DatalakeBucket",
-			{
-				description:
-					"Bucket to upload data for ingestion into BigQuery",
-			}
-		);
 
-		new GuStringParameter(
-			this,
-			"DeployBucket",
-			{
-				description:
-					"Bucket to copy files to",
-			}
-		);
+		// ---- Constants ---- //
+		const app = "support-reminders";
 
-		new GuStringParameter(
-			this,
-			"SecurityGroupToAccessPostgres",
-			{
-				description:
-					"Security group to access the RDS instance",
-			}
-		);
 
-		new GuStringParameter(
-			this,
-			"Stage",
-			{
-				description:
-					"Set by RiffRaff on each deploy",
-			}
-		);
-
-		new GuStringParameter(
-			this,
-			"Stack",
-			{
-				description:
-					"Stack name",
-			}
-		);
-
-		// ---- DNS Records ---- //
-		// new GuCname(this, id: string, props: GuCnameProps)
-
-		// ---- Lambdas ---- //
+		// ---- API-triggered lambda functions ---- //
 		const searchRemindersLambda = new GuLambdaFunction(this, "search-reminders", {
 			app: "search-reminders-lambda",
 			runtime: Runtime.NODEJS_14_X,
@@ -98,6 +60,47 @@ export class SupportReminders extends GuStack {
 			fileName: "support-reminders.zip",
 		});
 
+
+		// ---- API gateway ---- //
+		const supportRemindersApi = new GuApiGatewayWithLambdaByPath(this, {
+			app,
+			monitoringConfiguration: {
+				snsTopicName: "conversion-dev",
+				http5xxAlarm: {
+					tolerated5xxPercentage: 1,
+				}
+			},
+			targets: [
+				{
+					path: "/reactivate",
+					httpMethod: "POST",
+					lambda: reactivateRecurringReminderLambda,
+				},
+				{
+					path: "/create/recurring",
+					httpMethod: "POST",
+					lambda: createRemindersSignupLambda,
+				},
+				{
+					path: "/search",
+					httpMethod: "POST",
+					lambda: searchRemindersLambda,
+				},
+				{
+					path: "/create/one-off",
+					httpMethod: "POST",
+					lambda: createRemindersSignupLambda,
+				},
+				{
+					path: "/cancel",
+					httpMethod: "POST",
+					lambda: cancelRemindersLambda,
+				},
+			],
+		})
+
+
+		// ---- Scheduled lambda functions ---- //
 		new GuScheduledLambda(this, "signup-exports", {
 			app: "cancel-reminders-lambda",
 			runtime: Runtime.NODEJS_14_X,
@@ -130,41 +133,29 @@ export class SupportReminders extends GuStack {
 			},
 		});
 
-		new GuApiGatewayWithLambdaByPath(this, {
-			app: "support-reminders",
-			targets: [
+
+		// ---- DNS ---- //
+		const certificateArn = `arn:aws:acm:eu-west-1:${this.account}:certificate/${props.certificateId}`;
+
+		const cfnDomainName = new CfnDomainName(this, "ApiDomainName", {
+			domainName: props.domainName,
+			certificateArn,
+		});
+
+		new CfnBasePathMapping(this, "ApiMapping", {
+			domainName: cfnDomainName.ref,
+			restApiId: supportRemindersApi.api.restApiId,
+			stage: supportRemindersApi.api.deploymentStage.stageName,
+		});
+
+		new CfnRecordSetGroup(this, "ApiRoute53", {
+			hostedZoneId: props.hostedZoneId,
+			recordSets: [
 				{
-					path: "/reactivate",
-					httpMethod: "POST",
-					lambda: reactivateRecurringReminderLambda,
+					name: props.domainName,
+					type: "CNAME",
 				},
-				{
-					path: "/create/recurring",
-					httpMethod: "POST",
-					lambda: createRemindersSignupLambda,
-				},
-				{
-					path: "/search",
-					httpMethod: "POST",
-					lambda: searchRemindersLambda,
-				},
-				{
-					path: "/create/one-off",
-					httpMethod: "POST",
-					lambda: createRemindersSignupLambda,
-				},
-				{
-					path: "/cancel",
-					httpMethod: "POST",
-					lambda: cancelRemindersLambda,
-				}
 			],
-			monitoringConfiguration: {
-				snsTopicName: "conversion-dev",
-				http5xxAlarm: {
-					tolerated5xxPercentage: 1,
-				}
-			}
-		})
+		});
 	}
 }
