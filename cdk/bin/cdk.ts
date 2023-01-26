cec27b0d64214dce85b1c3dac8b79f189f47c1b7
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-01-26 14:14:03 +0000

    remove `datalakeBucket` parameter (2)

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -11,6 +11,7 @@ export const codeProps: SupportRemindersProps = {
 	certificateId: "b384a6a0-2f54-4874-b99b-96eeff96c009",
 	domainName: "reminders-code.support.guardianapis.com",
 	hostedZoneId: "Z3KO35ELNWZMSX",
+	datalakeBucket: "contributions-private",
 };
 
 export const prodProps: SupportRemindersProps = {
@@ -19,6 +20,7 @@ export const prodProps: SupportRemindersProps = {
 	certificateId: "b384a6a0-2f54-4874-b99b-96eeff96c009",
 	domainName: "reminders.support.guardianapis.com",
 	hostedZoneId: "Z3KO35ELNWZMSX",
+	datalakeBucket: "ophan-raw-support-reminders",
 };
 
 new SupportReminders(app, "SupportReminders-CODE", codeProps);
