ea5bf658e0273d1a1a14619f7f375f9127131c5a
Tom Forbes <tom.forbes@theguardian.com>
2024-12-17 14:55:12 +0000

    local

--- src/reactivate-recurring-reminder/lambda/local.ts
+++ src/reactivate-recurring-reminder/lambda/local.ts
@@ -1,31 +1,31 @@
-import * as AWS from 'aws-sdk';
-import { run } from './lambda';
-
-const config = new AWS.Config();
-const credentials = new AWS.SharedIniFileCredentials({ profile: 'membership' });
-config.update({ region: 'eu-west-1', credentials });
-
-function runLocal() {
-	console.log(__dirname);
-	process.env.Stage = 'DEV';
-
-	const event = {
-		path: '/reactivate',
-		headers: {},
-		body: JSON.stringify({
-			email: 'test-reminders10@theguardian.com',
-		}),
-	};
-
-	run(event)
-		.then((result) => {
-			console.log('============================');
-			console.log('Result: ', result);
-		})
-		.catch((err) => {
-			console.log('============================');
-			console.log('Failed to run locally: ', err);
-		});
-}
-
-runLocal();
+// import * as AWS from 'aws-sdk';
+// import { run } from './lambda';
+//
+// const config = new AWS.Config();
+// const credentials = new AWS.SharedIniFileCredentials({ profile: 'membership' });
+// config.update({ region: 'eu-west-1', credentials });
+//
+// function runLocal() {
+// 	console.log(__dirname);
+// 	process.env.Stage = 'DEV';
+//
+// 	const event = {
+// 		path: '/reactivate',
+// 		headers: {},
+// 		body: JSON.stringify({
+// 			email: 'test-reminders10@theguardian.com',
+// 		}),
+// 	};
+//
+// 	run(event)
+// 		.then((result) => {
+// 			console.log('============================');
+// 			console.log('Result: ', result);
+// 		})
+// 		.catch((err) => {
+// 			console.log('============================');
+// 			console.log('Failed to run locally: ', err);
+// 		});
+// }
+//
+// runLocal();
