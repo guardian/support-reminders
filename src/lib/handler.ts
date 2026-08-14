d22f083b6657f02bbd8faeeaf5a128781599314e
Juarez Mota <juarez.mota.mindera@guardian.co.uk>
2026-08-14 11:41:07 +0100

    Bump Node.js from 22.16.0 to 24.19.0

--- src/lib/handler.ts
+++ src/lib/handler.ts
@@ -2,7 +2,6 @@ import {
 	APIGatewayProxyEvent,
 	APIGatewayProxyHandler,
 	APIGatewayProxyResult,
-	Callback,
 	Context,
 	SQSBatchResponse,
 	SQSEvent,
@@ -11,24 +10,23 @@ import {
 
 const getHandler =
 	<INPUT, OUTPUT>(run: (event: INPUT) => Promise<OUTPUT>) =>
-	(event: INPUT, context: Context, callback: Callback<OUTPUT>): void => {
-		// setTimeout is necessary because of a bug in the node lambda runtime which can break requests to ssm
-		setTimeout(() => {
-			// If we do not set this then the lambda will wait 10secs before completing.
-			// This is because pg starts a 10sec timer for each new client (see idleTimeoutMillis in https://node-postgres.com/api/pool).
-			// `callbackWaitsForEmptyEventLoop = false` ensures the invocation ends immediately (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-context.html)
-			context.callbackWaitsForEmptyEventLoop = false;
+	async (event: INPUT, context: Context): Promise<OUTPUT> => {
+		// If we do not set this then the lambda will wait 10secs before completing.
+		// This is because pg starts a 10sec timer for each new client (see idleTimeoutMillis in https://node-postgres.com/api/pool).
+		// `callbackWaitsForEmptyEventLoop = false` ensures the invocation ends immediately (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-context.html)
+		context.callbackWaitsForEmptyEventLoop = false;
 
-			run(event)
-				.then((result) => {
-					console.log('Returning to client:', JSON.stringify(result));
-					callback(null, result);
-				})
-				.catch((err: Error) => {
-					console.log(`Error: ${err.message}`);
-					callback(err);
-				});
-		});
+		// setTimeout is necessary because of a bug in the node lambda runtime which can break requests to ssm
+		await new Promise((resolve) => setTimeout(resolve));
+
+		try {
+			const result = await run(event);
+			console.log('Returning to client:', JSON.stringify(result));
+			return result;
+		} catch (err) {
+			console.log(`Error: ${(err as Error).message}`);
+			throw err;
+		}
 	};
 
 export const getApiGatewayHandler = (
